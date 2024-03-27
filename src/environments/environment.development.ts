a03946318917c5e595b580311d518375466dd365
Paul Theby <paultheby@gmail.com>
2024-03-26 20:49:47 -0500

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  link:'http://localhost:3000',
+  name:'development environment',
+  code: 1005,
+ };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  link:'https://thebyagentpolicy.onrender.com',
+  name:'production environment',
+  code: 1004,
+};
